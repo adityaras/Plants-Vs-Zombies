800596f6d17d257b6581adc003d09977fb592b22
SiliconAlchemist <shrikant18312@iiitd.ac.in>
2019-11-01 18:18:07 +0530

    Credits & Instructions

--- Static_GUI/src/sample/Settings_Controller.java
+++ Static_GUI/src/sample/Settings_Controller.java
@@ -3,11 +3,22 @@ package sample;
 
 import javafx.fxml.FXML;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 
 public class Settings_Controller {
+    public static Stage Instruction_stage= new Stage(StageStyle.TRANSPARENT);
+    public static Stage Credits_stage= new Stage(StageStyle.TRANSPARENT);
+
     @FXML
     private ImageView instructions;
     @FXML
@@ -28,8 +39,16 @@ public class Settings_Controller {
     public void instruction_exited()
     { instructions.setImage(instructions_button); }
     @FXML
-    public void instruction_clicked()
-    {  }
+    public void instruction_clicked() throws IOException {
+        Controller.Settings_Stage.setOpacity(0.5);
+        //Instruction_stage.hide();
+        Instruction_stage.setTitle("Instructions Menu");
+        FXMLLoader fxmlloader =  new FXMLLoader();
+        fxmlloader.setLocation(Game_GUI.class.getResource("Instructions.fxml"));
+        Pane instruction_screen = (Pane) fxmlloader.load();
+        Instruction_stage.setScene(new Scene(instruction_screen, Color.TRANSPARENT));
+        Instruction_stage.showAndWait();
+    }
 
     @FXML
     public void credits_entered()
@@ -38,8 +57,16 @@ public class Settings_Controller {
     public void credits_exited()
     { credits.setImage(credits_button); }
     @FXML
-    public void credits_clicked()
-    {  }
+    public void credits_clicked() throws IOException
+    {   Controller.Settings_Stage.setOpacity(0.5);
+        //Instruction_stage.hide();
+        Credits_stage.setTitle("Credits Menu");
+        FXMLLoader fxmlloader =  new FXMLLoader();
+        fxmlloader.setLocation(Game_GUI.class.getResource("Credits.fxml"));
+        Pane credits_screen = (Pane) fxmlloader.load();
+        Credits_stage.setScene(new Scene(credits_screen, Color.TRANSPARENT));
+        Credits_stage.showAndWait();
+    }
 
     @FXML
     public void go_back_entered()
